ff8d1e8eb2744e68295b8105a116dd89db374267
minuhin <minuhin@restream.rt.ru>
2019-07-29 12:49:00 +0300

    add preprocessor_ext

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+# 1.0.2
+
+-    Added preprocessor_ext for better warnings (and better code)
+
 # 1.0.1
 
 -    Added 'element' option to customize anchor span element.
--- foliant/preprocessors/anchors.py
+++ foliant/preprocessors/anchors.py
@@ -4,13 +4,10 @@ Arbitrary anchors for Foliant.
 
 import re
 
-from foliant.preprocessors.base import BasePreprocessor
-from foliant.utils import output
-
 from foliant.preprocessors.utils.combined_options import (CombinedOptions,
                                                           boolean_convertor)
-
-OptionValue = int or float or bool or str
+from foliant.preprocessors.utils.preprocessor_ext import (BasePreprocessorExt,
+                                                          allow_fail)
 
 
 def convert_to_anchor(reference: str) -> str:
@@ -36,7 +33,28 @@ def convert_to_anchor(reference: str) -> str:
     return result.strip(' -')
 
 
-class Preprocessor(BasePreprocessor):
+def collect_header_anchors(text: str) -> str:
+    '''collect all headers in text and return dictionary {anchor: header}'''
+    pattern = r'^#+ (.+)'
+    headers = re.findall(pattern, text, re.MULTILINE)
+    return {convert_to_anchor(h): h for h in headers}
+
+
+def fix_headers(text: str) -> str:
+    '''add empty line after anchor if it goes before header'''
+    pattern = r'(<anchor>.+?</anchor>\n)(#)'
+    return re.sub(pattern, r'\1\n\2', text)
+
+
+def get_span_anchor(anchor: str, options: CombinedOptions) -> str:
+    return options['element'].format(anchor=anchor)
+
+
+def get_tex_anchor(anchor: str) -> str:
+    return r'\hypertarget{%s}{}' % anchor
+
+
+class Preprocessor(BasePreprocessorExt):
     defaults = {
         'tex': False,
         'element': '<span id="{anchor}"></span>'
@@ -46,49 +64,30 @@ class Preprocessor(BasePreprocessor):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.logger = self.logger.getChild('graphviz')
+        self.logger = self.logger.getChild('anchors')
 
         self.logger.debug(f'Preprocessor inited: {self.__dict__}')
 
-    def _collect_header_anchors(self, text):
-        '''collect all headers in text and return dictionary {anchor: header}'''
-        pattern = r'^#+ (.+)'
-        headers = re.findall(pattern, text, re.MULTILINE)
-        if not headers:
-            return {}
-        return {convert_to_anchor(h): h for h in headers}
-
-    def _fix_headers(self, text):
-        '''adds empty line after anchor if it goes before header'''
-        pattern = r'(<anchor>.+?</anchor>\n)(#)'
-        return re.sub(pattern, r'\1\n\2', text)
-
-    def _get_span_anchor(self, anchor: str, options: CombinedOptions) -> str:
-        return options['element'].format(anchor=anchor)
-
-    def _get_tex_anchor(self, anchor: str) -> str:
-        return r'\hypertarget{%s}{}' % anchor
-
+    @allow_fail()
     def process_anchors(self, content: str) -> str:
-
         def _sub(block) -> str:
             anchor = block.group('body').strip()
             if anchor in self.applied_anchors:
-                output(f"WARNING: Can't apply dublicate anchor \"{anchor}\", skipping.",
-                       quiet=self.quiet)
+                self._warning(f"Can't apply dublicate anchor \"{anchor}\", skipping.",
+                              context=self.get_tag_context(block))
                 return ''
             if anchor in self.header_anchors:
-                output(f'WARNING: anchor "{anchor}" may conflict with header '
-                       f'"{self.header_anchors[anchor]}".', quiet=self.quiet)
+                self._warning(f'anchor "{anchor}" may conflict with header "{self.header_anchors[anchor]}".',
+                              context=self.get_tag_context(block))
             options = CombinedOptions({'main': self.options,
                                        'tag': self.get_options(block.group('options'))},
                                       convertors={'tex': boolean_convertor},
                                       priority='tag')
             self.applied_anchors.append(anchor)
             if self.context['target'] == 'pdf' and options['tex']:
-                return self._get_tex_anchor(anchor)
+                return get_tex_anchor(anchor)
             else:
-                return self._get_span_anchor(anchor, options)
+                return get_span_anchor(anchor, options)
         return self.pattern.sub(_sub, content)
 
     def apply(self):
@@ -98,10 +97,10 @@ class Preprocessor(BasePreprocessor):
             with open(markdown_file_path, encoding='utf8') as markdown_file:
                 content = markdown_file.read()
 
-            content = self._fix_headers(content)
+            content = fix_headers(content)
 
             self.applied_anchors = []
-            self.header_anchors = self._collect_header_anchors(content)
+            self.header_anchors = collect_header_anchors(content)
 
             processed = self.process_anchors(content)
 
